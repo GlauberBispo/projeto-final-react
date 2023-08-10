cd4a148920a4da5c9e8fd630855ddaf45d4c311e
Monara Calado <monaracalado@gmail.com>
2023-08-09 23:48:32 -0300

    ajustando hover menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -6,9 +6,9 @@ export default function Menu(){
     return(
         <>
         <nav className='linkMenuPrincipal'>
-            <MenuBase to='/'>Home</MenuBase>
-            <MenuBase to='/Produtos'>Produtos</MenuBase>
-            <MenuBase to='/Contato'>Contato</MenuBase>
+            <MenuBase className="btnSimple" to='/'>Home</MenuBase>
+            <MenuBase className="btnSimple" to='/Produtos'>Produtos</MenuBase>
+            <MenuBase className="btnSimple" to='/Contato'>Contato</MenuBase>
             <MenuBase className="btnCustom" to='/EntreCadastre-se'>Entre | Cadastre-se</MenuBase>
             
         </nav>
