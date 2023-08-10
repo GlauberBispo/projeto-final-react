426c2331166eb5ca4f16359c796a20abe01bf141
Glauber Bispo <glauber.bispo@gmail.com>
2023-08-10 15:40:46 -0300

    concluída estilização da home e componentes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import BigHero from '../../components/bighero';
 import Beneficios from '../../components/beneficios';
 import Depoimentos from '../../components/depoimento';
-import Faq from '../../components/faq'; // Importe o componente Faq
+import Faq from '../../components/faq'; 
 
 export default function Home() {
-    const apiKey = 'iusTK4QD';
+    
     
   return (
     <>
       <BigHero />
       <Beneficios />
       <Depoimentos />
-      <Faq apiKey={apiKey}/> 
+      <Faq /> 
     </>
   );
 }
