180903d03713e4b6f0052e816669195b07741643
Monara Calado <monaracalado@gmail.com>
2023-08-11 04:30:03 -0300

    wip

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -2,30 +2,14 @@
 import MenuBase from "../menuBase";
 import './styleMenu.css'
 
-interface MenuProps {
-    isLoggedIn: boolean;
-}
-
-export default function Menu({isLoggedIn}: MenuProps){
+export default function Menu(){
     return(
         <>
         <nav className='linkMenuPrincipal'>
-            {isLoggedIn ? (
-                <>
-                    <MenuBase className="btnSimple" to='/'>Home</MenuBase>
-                    <MenuBase className="btnSimple" to='/produtos'>Produtos</MenuBase>
-                    <MenuBase className="btnSimple" to='/contato'>Contato</MenuBase>
-                    <MenuBase className="btnSimple" to='/carrinho'><i className="ph ph-shopping-cart-simple icon"></i></MenuBase>
-                    <MenuBase className="btnSimple" to='/'><i className="ph ph-sign-out icon"></i></MenuBase>
-                </>
-            ):(
-                <>
-                    <MenuBase className="btnSimple" to='/'>Home</MenuBase>
-                    <MenuBase className="btnSimple" to='/loginCadastro'>Produtos</MenuBase>
-                    <MenuBase className="btnSimple" to='/loginCadastro'>Contato</MenuBase>
-                    <MenuBase className="btnCustom" to='/loginCadastro'>Entre | Cadastre-se</MenuBase>
-                </>
-            )}
+            <MenuBase className="btnSimple" to='/'>Home</MenuBase>
+            <MenuBase className="btnSimple" to='/loginCadastro'>Produtos</MenuBase>
+            <MenuBase className="btnSimple" to='/loginCadastro'>Contato</MenuBase>
+            <MenuBase className="btnCustom" to='/loginCadastro'>Entre | Cadastre-se</MenuBase>
         </nav>
         </>
     )
